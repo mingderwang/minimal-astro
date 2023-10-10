225a35aa0a0a39be6e0e3de24320db881e409d17
Ming-der Wang <mingderwang@gmail.com>
2023-10-10 22:12:56 +0800

    svg license from Vectornator

---
+++ src/components/Counter.svelte
@@ -0,0 +1,23 @@
+<!-- Counter.svelte -->
+<script>
+    let count = 0;
+  
+    function increment() {
+      count += 1;
+    }
+  
+    function decrement() {
+      count -= 1;
+    }
+  </script>
+  
+  <style>
+
+  </style>
+  
+  <div class="counter">
+    <p>Count: {count}</p>
+    <button on:click={increment}>Increment</button>
+    <button on:click={decrement}>Decrement</button>
+  </div>
+  
