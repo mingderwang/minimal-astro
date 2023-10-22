e0265ce1c5d3f20f210952d2ddba56eabb64c924
Ming-der Wang <mingderwang@gmail.com>
2023-10-22 14:25:07 +0800

    support vercel and radix-ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
 import react from "@astrojs/react";
-
+import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    ssr: {
+      noExternal: [/^@radix-ui\/*/]
+    }
+  },
+  adapter: vercel(),
   integrations: [svelte(), react(), tailwind({
       applyBaseStyles: false,
     })],
